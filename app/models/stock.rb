72365021d95937c183f4fe9f6efb20a3743f84c1
andydenenberg <andydenenberg@gmail.com>
2018-03-30 16:22:18 -0500

    added portfolios

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,5 @@
 class Stock < ApplicationRecord
-  belongs_to :portfolio
+#  belongs_to :portfolio
 end
 
 #   rails g scaffold Stock portfolio:references purchase_price:decimal quantity:decimal symbol:string name:string purchase_date:string strike:decimal expiration_date:string stock_option:string
