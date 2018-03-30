b4880a9fd6aff33370c1fd2bd99f1f712144767c
andydenenberg <andydenenberg@gmail.com>
2018-03-30 16:16:20 -0500

    added portfolios

--- db/migrate/20180330174616_create_stocks.rb
+++ db/migrate/20180330174616_create_stocks.rb
@@ -1,12 +1,12 @@
 class CreateStocks < ActiveRecord::Migration[5.1]
   def change
     create_table :stocks do |t|
+      t.integer :portfolio_id
       t.references :portfolio, foreign_key: true
       t.decimal :purchase_price,  :precision => 10, :scale => 2
       t.decimal :quantity,        :precision => 10, :scale => 2
       t.string :symbol
       t.string :name
-      t.integer :portfolio_id
       t.string :purchase_date
       t.decimal :strike,          :precision => 10, :scale => 2
       t.string :expiration_date
