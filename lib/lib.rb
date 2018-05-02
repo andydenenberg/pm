45af65486219d29c5bc03d5293ee518c06317d4c
andydenenberg <andydenenberg@gmail.com>
2018-05-02 17:01:36 -0500

    multiple apps

--- lib/lib.rb
+++ lib/lib.rb
@@ -1,17 +1,16 @@
 module Lib
   
   def self.translate_groupids(portfolio_name)
-    
       if Portfolio.find_by_name(portfolio_name).nil?
         # not an individual portfolio
         case portfolio_name
         when 'All Portfolios' 
           portfolio_ids = Portfolio.all.collect { |p| p.id }
-        else # individual portfolios
-          portfolio_ids = Portfolio.find_by_name(portfolio_name).collect { |p| p.id }
+        else # Group ofportfolios
+          portfolio_ids = Group.find_by_name(portfolio_name).portfolios.collect { |p| p.id }
         end
-      else # Group of portfolios
-        portfolio_ids = Group.find_by_name(portfolio_name).portfolios.collect { |p| p.id }
+      else #individual portfolios
+          portfolio_ids = Portfolio.find_by_name(portfolio_name).id
       end
     return portfolio_ids
   end
