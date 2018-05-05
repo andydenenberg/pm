ff8d70b1b97392080aef019d050484ddc99901ff
andydenenberg <andydenenberg@gmail.com>
2018-05-05 17:34:22 -0500

    time stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
-  @ironcache = IronCache::Client.new
-  @cache = @ironcache.cache("my_cache")
-  @pr_time = Time.parse(@cache.get("poll_request_time").value)
-  
-  puts
-  puts
-  puts 'here'
-  puts @pr_time
-  puts
-  
 end
