37894994616c0e2b7acaa1a25b435c857634ba1a
andydenenberg <andydenenberg@gmail.com>
2018-05-06 10:03:12 -0500

    time stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  before_action :poll_request #, except: [:create, :update, :destroy]
+  
+  def poll_request
+    ironcache = IronCache::Client.new 
+    @cache = ironcache.cache("my_cache")  
+  end
+  
 end
