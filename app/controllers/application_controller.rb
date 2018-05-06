a7fd044a230d35bfcf02363511ca5fa81e806bfc
andydenenberg <andydenenberg@gmail.com>
2018-05-06 11:03:00 -0500

    time stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   def poll_request
     ironcache = IronCache::Client.new 
     cache = ironcache.cache("my_cache")
-    @poll_request_time = Time.parse(cache.get("poll_request_time").value)+5.hours
+    @poll_request_time = Time.parse(cache.get("poll_request_time").value) #+5.hours
   end
   
 end
