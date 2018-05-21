1e9f58a9885e0ec96f030ebaf03f9d58178ac08c
andydenenberg <andydenenberg@gmail.com>
2018-05-21 10:02:27 -0500

    added divs to home

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -61,10 +61,12 @@ function ToggleActivePoll(stock_option) {
 if (poll) { 
 	poll = false ;
 	$("#poll_active_button").removeClass("btn-success");  
-	$("#poll_active_button").addClass("btn-danger");  			
+	$("#poll_active_button").addClass("btn-danger");
+	$('#poll_active_dot').find("path, polygon, circle").attr("fill", "red");
 	} else { 
 	poll = true
 	$("#poll_active_button").removeClass("btn-danger");  
 	$("#poll_active_button").addClass("btn-success");  			
+	$('#poll_active_dot').find("path, polygon, circle").attr("fill", "green");
 	};
 };
