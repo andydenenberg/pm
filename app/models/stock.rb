a4a8a9e24b3ce907c7fd2718c838400e8557b6fe
andydenenberg <andydenenberg@gmail.com>
2018-06-08 13:13:54 -0500

    login/logout

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -31,7 +31,7 @@ class Stock < ApplicationRecord
       symbols = stocks_funds.distinct.pluck(:symbol)
       values = symbols.collect { |sym| [ sym, stocks_funds.where(symbol: sym).sum(0) { |data| (data.quantity * (data.price ||= 0) ).to_f }] }
       total_value = values.sum(0) { |sym, value| value }.to_f
-      change = symbols.collect { |sym| [ sym, stocks_funds.where(symbol: sym).sum(0) { |data| (data.quantity * data.change).to_f }] }
+      change = symbols.collect { |sym| [ sym, stocks_funds.where(symbol: sym).sum(0) { |data| (data.quantity * (data.change ||= 0) ).to_f }] }
       total_change = change.sum(0) { |sym, value| value }.to_f
 
       stocks = values.collect { |sym, value| [ sym, 
