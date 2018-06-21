89f9f4aa035fa67d9348e38410ff26d7a7997d66
andydenenberg <andydenenberg@gmail.com>
2018-06-21 12:10:03 -0500

    clean up

--- config/routes.rb
+++ config/routes.rb
@@ -19,11 +19,11 @@ Rails.application.routes.draw do
   get 'graphs', to: 'home#graphs'
   get 'highlights', to: 'home#highlights'
   get 'dividends_new', to: 'home#dividends'
-  get 'poll_check_new', to: 'home#poll_check' 
+  get 'poll_check', to: 'home#poll_check' 
   get 'poll_set', to: 'home#poll_set' 
   
 #  get 'demo', to: 'home#demo'
-  get 'home', to: 'home#index'
+#  get 'home', to: 'home#index'
   get 'info', to: 'home#info' 
 #  get 'poll_check', to: 'portfolios#poll_check' 
 #  get 'dividends', to: 'stocks#dividends'
