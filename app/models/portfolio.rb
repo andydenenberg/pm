d620d9687779f568ee014c8388df79c859078e25
andydenenberg <andydenenberg@gmail.com>
2019-06-15 14:22:48 -0400

    Changed all to Funds(yahoo)

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -24,7 +24,7 @@ class Portfolio < ApplicationRecord
     total_stocks_change = all.sum { |s| s.total_stocks_change_value }
     total_options = all.sum { |s| s.total_options_value }
     total_dividends = all.sum { |s| s.total_dividends_value }
-    last_update = Stock.where(stock_option: 'Stock').last.updated_at
+    last_update = Stock.where(stock_option: 'Fund').last.updated_at
     return [ordered, total_cash, total_stocks, total_options, total_stocks_change, last_update, total_dividends ]      
   end
   
